83fbe2d629e6ce20d19b5c079b76b8b690caadae
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-04-22 13:40:47 -0300

    web project

---
+++ usuarios/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    
+    pass
+
+def logout(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def cadastro(request):
+    pass
